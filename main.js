2be1662b91b8b2f202d0be879812335bd52551e6
valeriilogvin <valerii.logvin@gmail.com>
2020-07-04 14:31:08 +0300

    create base

--- main.js
+++ main.js
@@ -13,3 +13,39 @@
     });
   });
 })(jQuery);
+$('.food-of-the-day').slick({
+  infinite: true,
+  lazyLoad: 'ondemand',
+  slidesToShow: 3,
+  draggable: false,
+  dotsClass: 'my-slick-dots',
+  prevArrow: '<button type="button" class="my-slick-prev"><svg class="icon icon-arrow-l"><use xlink:href="#icon-arrow-l"></use></svg></button>',
+  nextArrow: '<button type="button" class="my-slick-next"><svg class="icon icon-arrow-r"><use xlink:href="#icon-arrow-r"></use></svg></button>',
+  dots: true,
+  slidesToScroll: 1,
+  // autoplay: true,
+  responsive: [
+    {
+      breakpoint: 1001,
+      settings: {
+        slidesToShow: 2
+
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+        infinite: true
+      }
+    },
+    {
+      breakpoint: 641,
+      settings: {
+        slidesToShow: 1,
+        infinite: true
+      }
+    }
+  ]
+});
+
